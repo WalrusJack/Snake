ec26c4487f257df776856af6fc0b9b8a6e5fc783
WalrusJack <86444375+WalrusJack@users.noreply.github.com>
2021-07-03 23:10:59 -0400

    Completion of part 1

--- snake.c
+++ snake.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <sys/time.h>
 
 // Author: Bruno Francsico
@@ -11,19 +12,64 @@
 struct segment {
     int x;
     int y;
-    int xDir;           // xDir = 1 --> right, xDir = -1 --> left
-    int yDir;           // yDir = 1 --> down, yDir = -1 --> up
+    int xDir;               // xDir = 1 --> right, xDir = -1 --> left
+    int yDir;               // yDir = 1 --> down, yDir = -1 --> up
 };
 
+void moveSnake(int);
+void drawSnake(struct segment *);
+int addSegments(struct segment *, const int, int, int);
+void setTimer(long);
+
 WINDOW *window;
-int pitWidth;
-int pitHeight;
+int pitWidth;               // Width of the snake pit including the borders
+int pitHeight;              // Height of the snake pit including the borders
 
 struct segment *snake;
-int maxLength;
-int snakeSize;
+int maxLength;              // The longest the snake can be and the condition for winning
+int snakeSize;              // Current size of the snake
+
+long moveBase = 2000;       // Used to caculate how fast the snake will move
 
 int main() {
+    // Author: Bruno Francisco
+    // Setup the screen
+    window = initscr();
+    clear();
+    noecho();
+    cbreak();
+    curs_set(false);
+    keypad(window, true);
+    
+    // Snake will move after a certain period of time has passed
+    signal(SIGALRM, moveSnake);
+
+    // Snake pit takes up the entire screen size
+    pitWidth = COLS;
+    pitHeight = LINES;
+
+    // drawBorder(pitWidth, pitHeight);
+    box(window, pitHeight, pitHeight);
+
+    // Initialize the snake
+    maxLength = pitWidth + pitHeight;
+    snake = malloc(sizeof(*snake) * maxLength);
+    snake[0].x = pitWidth / 2;
+    snake[0].y = pitHeight / 2;
+    snake[0].xDir = 1;
+    snake[0].yDir = 0;
+    int initSize = 5;
+    snakeSize = addSegments(snake, maxLength, 1, initSize - 1);
+    drawSnake(snake);
+    
+    // Draw the screen
+    refresh();
+    
+    // Determine how fast the snake will move
+    // Snake speed depends on its size
+    long moveRate = moveBase / snakeSize;
+    setTimer(moveRate);
+    
     // Author: Bruno Francisco
     // Check for user input
     // If the user presses 'q' or 'Q' the game quits
@@ -55,6 +101,11 @@ int main() {
                 break;
         }
     }
+    
+    free(snake);        // Free the space taken up by the snake array
+    endwin();           // Restore the screen and quit the game
+    
+    return 0;
 }
 
 // Author: Bruno Francisco
@@ -62,19 +113,9 @@ int main() {
 // and checks for collision between the borders and snake body
 // If collsion is detected, print a game over screen
 void moveSnake(int signum) {
-    // Characters to be used to display the head and body of the snake
-    const int headChar = '#';
-    const int bodyChar = '#';
-    
-    // Delete the old tail of the snake
-    move(snake[snakeSize - 1].y, snake[snakeSize - 1].x);
-    addch(' ');
-
     // Move the head of the snake into its new position
     snake[0].x += snake[0].xDir;
     snake[0].y += snake[0].yDir;
-    move(snake[0].y, snake[0].x);
-    addch(headChar);
 
     // Update the position and direction of each segment in the snake
     for(int i = snakeSize - 1; i > 0; i--) {
@@ -82,10 +123,11 @@ void moveSnake(int signum) {
         snake[i].y += snake[i].yDir;
         snake[i].xDir = snake[i - 1].xDir;
         snake[i].yDir = snake[i - 1].yDir;
-        move(snake[i].y, snake[i].x);
-        addch(bodyChar);
     }
-
+    
+    // Draw the snake
+    drawSnake(snake);
+    
     // Check to see if the snake head has collided with the body
     for(int i = 1; i < snakeSize; i++)                      
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y)
@@ -93,11 +135,33 @@ void moveSnake(int signum) {
     
     // Check to see if the snake head has collided with the pit walls
     if(snake[0].x <= 0 || snake[0].x >= pitWidth - 1 || snake[0].y <= 0 || snake[0].y >= pitHeight - 1)
-        exit(0)
+        exit(0);
 
     // Place cursor in a non-intrusive place and update the screen
     move(pitHeight - 1, pitWidth - 1);
     refresh();
+}
+
+// Bruno Francisco
+// Draws the snake on the screen but does not refresh it
+void drawSnake(struct segment *snake) {
+    // Characters to be used to display the head and body of the snake
+    const int headChar = '#';
+    const int bodyChar = '#';
+
+    // Draw the head
+    move(snake[0].y, snake[0].x);
+    addch(headChar);
+
+    // Draw the body
+    for(int i = 1; i < snakeSize - 1; i++) {
+        move(snake[i].y, snake[i].x);
+        addch(bodyChar);
+    }
+
+    // Delete the old tail of the snake
+    move(snake[snakeSize - 1].y, snake[snakeSize - 1].x);
+    addch(' ');
 }
 
 // Author: Bruno Francisco
@@ -131,6 +195,7 @@ void setTimer(long time) {
     setitimer(ITIMER_REAL, &timer, NULL);       // Set the timer
 }
 
+/* ::UNUSED::
 // Author: Kobe Onye
 // Draws the border and the background for the snake to move in
 void border(int height, int width)
@@ -140,4 +205,4 @@ void border(int height, int width)
     WINDOW *board_win = newwin(height, width, (yMax/2) - (height/2), (xMax/2) - (width/2)); // Creates the border 
     box(board_win, 0, 0);                                                                   // Draws a straight line to be used as the snake border
     wrefresh(board_win);
-}
+}*/
